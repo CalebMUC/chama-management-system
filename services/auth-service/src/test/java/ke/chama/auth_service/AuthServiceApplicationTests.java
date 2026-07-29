0931de0d0efc83e10fa87c56c91d3d5820a2fe44
CalebMUC <muchiricaleb05@gmail.com>
2026-07-29 06:01:54 +0300

    Auth Service Implementation"

---
+++ services/auth-service/src/test/java/ke/chama/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.chama.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
